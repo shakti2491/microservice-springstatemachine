b9b5858fd34d06e2fe84bb7523f3235f768386f9
shakti singh <shakti.singh@copart.com>
2020-12-13 20:45:44 -0600

    Spring State Machine

---
+++ src/main/java/shakti/microservices/microservicespringstatemachine/MicroserviceSpringstatemachineApplication.java
@@ -0,0 +1,13 @@
+package shakti.microservices.microservicespringstatemachine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceSpringstatemachineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceSpringstatemachineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/shakti/microservices/microservicespringstatemachine/MicroserviceSpringstatemachineApplicationTests.java
@@ -0,0 +1,13 @@
+package shakti.microservices.microservicespringstatemachine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceSpringstatemachineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
